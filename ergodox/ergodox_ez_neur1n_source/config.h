102427596c9c6cef0c1864a67ea642cfaa2fd45e
Neur1n <Jihang.Li@outlook.com>
2021-03-07 23:39:04 +0800

    chore(ergodox): remove caps lock, add file manager and calculator shortcut

--- ergodox/ergodox_ez_neur1n_source/config.h
+++ ergodox/ergodox_ez_neur1n_source/config.h
@@ -3,3 +3,4 @@
   See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
 */
 #define ORYX_CONFIGURATOR
+#define USB_SUSPEND_WAKEUP_DELAY 0
--- ergodox/ergodox_ez_neur1n_source/keymap.c
+++ ergodox/ergodox_ez_neur1n_source/keymap.c
@@ -49,12 +49,12 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ergodox_pretty(
-    KC_GRAVE,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           LGUI(KC_SPACE),                                 KC_CAPSLOCK,    KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_BSLASH,
+    KC_GRAVE,       KC_1,           KC_2,           KC_3,           KC_4,           KC_5,           LGUI(KC_SPACE),                                 TT(1),          KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_BSLASH,
     KC_TAB,         KC_Q,           KC_W,           KC_E,           KC_R,           KC_T,           KC_PGUP,                                        KC_LBRACKET,    KC_Y,           KC_U,           KC_I,           KC_O,           KC_P,           KC_QUOTE,
     KC_LCTRL,       KC_A,           KC_S,           KC_D,           KC_F,           KC_G,                                                                           KC_H,           KC_J,           KC_K,           KC_L,           KC_SCOLON,      KC_MINUS,
     KC_LALT,        KC_Z,           KC_X,           KC_C,           KC_V,           KC_B,           KC_PGDOWN,                                      KC_RBRACKET,    KC_N,           KC_M,           KC_COMMA,       KC_DOT,         KC_SLASH,       KC_EQUAL,
     KC_ESCAPE,      KC_HOME,        KC_END,         KC_DELETE,      KC_BSPACE,                                                                                                      KC_LEFT,        KC_DOWN,        KC_UP,          KC_RIGHT,       KC_LGUI,
-                                                                                                    TT(1),          KC_TRANSPARENT, KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,
+                                                                                                    KC_MY_COMPUTER, KC_CALCULATOR,  KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,
                                                                                                                     KC_PC_PASTE,    KC_PSCREEN,
                                                                                     KC_SPACE,       KC_LSHIFT,      KC_PC_COPY,     KC_INSERT,      KC_RSHIFT,      KC_ENTER
   ),
@@ -63,8 +63,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_KP_7,        KC_KP_8,        KC_KP_9,        KC_KP_0,        KC_TRANSPARENT, KC_TRANSPARENT,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                 KC_KP_4,        KC_KP_5,        KC_KP_6,        KC_TRANSPARENT, KC_KP_ASTERISK, KC_KP_MINUS,
     KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_KP_1,        KC_KP_2,        KC_KP_3,        KC_KP_DOT,      KC_KP_SLASH,    KC_KP_PLUS,
-    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_UP,       KC_MS_RIGHT,    KC_TRANSPARENT,
-                                                                                                    KC_TRANSPARENT, RESET,          KC_TRANSPARENT, KC_TRANSPARENT,
+    KC_TRANSPARENT, KC_TRANSPARENT, RESET,          KC_TRANSPARENT, KC_TRANSPARENT,                                                                                                 KC_MS_LEFT,     KC_MS_DOWN,     KC_MS_UP,       KC_MS_RIGHT,    KC_TRANSPARENT,
+                                                                                                    KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                                                     KC_TRANSPARENT, KC_TRANSPARENT,
                                                                                     KC_MS_BTN1,     KC_MS_BTN2,     KC_TRANSPARENT, KC_TRANSPARENT, KC_MS_WH_DOWN,  KC_MS_WH_UP
   ),
